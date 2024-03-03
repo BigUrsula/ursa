369db2a1d969de7486003d0b69ee7e9f1787e3c3
BigUrsula <160300351+BigUrsula@users.noreply.github.com>
2024-03-02 18:55:54 -0800

    Update

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -387,14 +387,14 @@ public:
         strNetworkID = "main";
         consensus.nSubsidyHalvingInterval = 210240; // Note: actual number of blocks per calendar year with DGW v3 is ~200700 (for example 449750 - 249050)
         consensus.nMasternodePaymentsStartBlock = 4010; // not true, but it's ok as long as it's less then nMasternodePaymentsIncreaseBlock
-        consensus.nMasternodePaymentsIncreaseBlock = 1735; // actual historical value
+        consensus.nMasternodePaymentsIncreaseBlock = 2530333; // actual historical value
         consensus.nMasternodePaymentsIncreasePeriod = 576*30; // 17280 - actual historical value
         consensus.nInstantSendConfirmationsRequired = 6;
         consensus.nInstantSendKeepLock = 24;
-        consensus.nBudgetPaymentsStartBlock = 1735; // actual historical value
+        consensus.nBudgetPaymentsStartBlock = 2530333; // actual historical value
         consensus.nBudgetPaymentsCycleBlocks = 16616; // ~(60*24*30)/2.6, actual number of blocks per month is 200700 / 12 = 16725
         consensus.nBudgetPaymentsWindowBlocks = 100;
-        consensus.nSuperblockStartBlock = 1736; //
+        consensus.nSuperblockStartBlock = 2530333; //
         consensus.nSuperblockStartHash = uint256();
         consensus.nSuperblockCycle = 16616; // ~(60*24*30)/2.6, actual number of blocks per month is 200700 / 12 = 16725
         consensus.nGovernanceMinQuorum = 5;
